e515c6dd86a51a28303d27034a5642eb661b3e42
Дмитрий <dima@ezoport.com>
2022-07-31 21:35:41 +0300

    add models_posts_app

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from django.urls import path
+from rest_framework.authtoken import views
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
+router.register('groups', GroupViewSet, basename='group')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
 ]
